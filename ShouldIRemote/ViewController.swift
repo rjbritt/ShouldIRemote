5f568f2b85f599510fac46bea0bd69082bf670d9
Ryan Britt <rjbritt071@gmail.com>
2015-08-28 20:22:53 -0400

    Initial Commit

---
+++ ShouldIRemote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShouldIRemote
+//
+//  Created by Ryan Britt on 8/28/15.
+//  Copyright (c) 2015 Irrational Midnight Avengers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
